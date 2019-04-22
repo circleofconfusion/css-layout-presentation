be0dc742cf09c76cd6771fcecc1419785b2a1749
Shane Knudsen <shaneknu@gmail.com>
2019-04-21 23:55:58 -0400

    Advantages and disadvantages of table layout

--- pages.js
+++ pages.js
@@ -1,5 +1,7 @@
 var pages = [
     'index.html',
     'contents.html',
-    'history-table-layout.html'
+    'history-table-layout.html',
+    'history-table-layout-advantages.html',
+    'history-table-layout-disadvantages.html'
 ];
